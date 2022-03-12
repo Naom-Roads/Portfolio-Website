e51f00bcbf40f63a840966c33b36c38d522b5df6
Naom-Roads <nrrodrig@gmail.com>
2022-03-12 14:03:59 -0800

    Removed contact form and started contact modal on index, fixed issue with bootstrap

--- js/mainscripts.js
+++ js/mainscripts.js
@@ -1,70 +1,93 @@
-(function () {
-    let form = document.querySelector("#contact-form");
-    let emailInput = document.querySelector("#email");
-    let passwordInput = document.querySelector("#password");
+var contactModal = document.getElementById('contact')
+contactModal.addEventListener('show.bs.modal', function (event) {
+    // Button that triggered the modal
+    var button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    var recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an AJAX request here
+    // and then do the updating in a callback.
+    //
+    // Update the modal's content.
+    var contactTitle = contactModal.querySelector('.modal-title')
+    var modalBodyInput = contactModal.querySelector('.modal-body input')
 
-    function showErrorMessage(input, message) {
-        let container = input.parentElement;
-
-        let error = container.querySelector(".error-message");
-        if (error) {
-            container.removeChild(error);
-        }
-
-        // Adds Error Message if message is not empty
-
-        if (message) {
-            let error = document.createElement("div");
-            error.classList.add('error-message');
-            error.innerText = message;
-            container.appendChild(error);
-        }
-    }
-
-    function validateEmail() {
-        let value = emailInput.value;
-
-        if (!value) {
-            showErrorMessage(emailInput, 'Email is a required field.');
-            return false;
-        }
-
-        if (value.indexOf('@') === -1) {
-            showErrorMessage(emailInput, 'You must enter a valid email address.');
-            return false;
-        }
-
-        showErrorMessage(emailInput, null);
-        return true;
-    }
+    contactTitle.textContent = 'New message to ' + recipient
+    modalBodyInput.value = recipient
+})
 
 
-    function validatePassword() {
-        let value = passwordInput.value;
 
-        if (!value) {
-            showErrorMessage(passwordInput, 'The password needs to be at least 8 characters long.');
-            return false;
-        }
 
-        showErrorMessage(passwordInput, null);
-        return true;
-    }
 
-    function validateForm() {
-        let isValidEmail = validatePassword();
-        let isValidPassword = validatePassword();
 
-        emailInput.input.addEventListener('input', validateEmail);
-        passwordInput.addEventListener('input', validatePassword);
-        return isValidEmail && isValidPassword;
-    }
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        if (validateForm()) {
-            alert('Success!');
-        }
-    });
-})();
+// (function () {
+//     let form = document.querySelector("#contact-form");
+//     let emailInput = document.querySelector("#email");
+//     let passwordInput = document.querySelector("#password");
+//
+//     function showErrorMessage(input, message) {
+//         let container = input.parentElement;
+//
+//         let error = container.querySelector(".error-message");
+//         if (error) {
+//             container.removeChild(error);
+//         }
+//
+//         // Adds Error Message if message is not empty
+//
+//         if (message) {
+//             let error = document.createElement("div");
+//             error.classList.add('error-message');
+//             error.innerText = message;
+//             container.appendChild(error);
+//         }
+//     }
+//
+//     function validateEmail() {
+//         let value = emailInput.value;
+//
+//         if (!value) {
+//             showErrorMessage(emailInput, 'Email is a required field.');
+//             return false;
+//         }
+//
+//         if (value.indexOf('@') === -1) {
+//             showErrorMessage(emailInput, 'You must enter a valid email address.');
+//             return false;
+//         }
+//
+//         showErrorMessage(emailInput, null);
+//         return true;
+//     }
+//
+//
+//     function validatePassword() {
+//         let value = passwordInput.value;
+//
+//         if (!value) {
+//             showErrorMessage(passwordInput, 'The password needs to be at least 8 characters long.');
+//             return false;
+//         }
+//
+//         showErrorMessage(passwordInput, null);
+//         return true;
+//     }
+//
+//     function validateForm() {
+//         let isValidEmail = validatePassword();
+//         let isValidPassword = validatePassword();
+//
+//         emailInput.input.addEventListener('input', validateEmail);
+//         passwordInput.addEventListener('input', validatePassword);
+//         return isValidEmail && isValidPassword;
+//     }
+//
+//     form.addEventListener('submit', (e) => {
+//         e.preventDefault();
+//         if (validateForm()) {
+//             alert('Success!');
+//         }
+//     });
+// })();
 
